9e412f80e96cd45408d485cf0005fe07ead0f043
lieto448 <slavikhrina@gmail.com>
2024-04-16 22:41:22 +0200

    include na sidebar

--- manage-users.php
+++ manage-users.php
@@ -24,40 +24,8 @@
     <![endif]-->
 
   </head>
-  <body>  
-    <!-- Left column -->
-    <div class="templatemo-flex-row">
-      <div class="templatemo-sidebar">
-        <header class="templatemo-site-header">
-          <div class="square"></div>
-          <h1>Visual Admin</h1>
-        </header>
-        <div class="profile-photo-container">
-          <img src="images/profile-photo.jpg" alt="Profile Photo" class="img-responsive">  
-          <div class="profile-photo-overlay"></div>
-        </div>      
-        <!-- Search box -->
-        <form class="templatemo-search-form" role="search">
-          <div class="input-group">
-              <button type="submit" class="fa fa-search"></button>
-              <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">           
-          </div>
-        </form>
-        <div class="mobile-menu-icon">
-            <i class="fa fa-bars"></i>
-          </div>
-        <nav class="templatemo-left-nav">          
-          <ul>
-            <li><a href="index.php"><i class="fa fa-home fa-fw"></i>Dashboard</a></li>
-            <li><a href="data-visualization.php"><i class="fa fa-bar-chart fa-fw"></i>Charts</a></li>
-            <li><a href="data-visualization.php"><i class="fa fa-database fa-fw"></i>Data Visualization</a></li>
-            <li><a href="maps.php"><i class="fa fa-map-marker fa-fw"></i>Maps</a></li>
-            <li><a href="#" class="active"><i class="fa fa-users fa-fw"></i>Manage Users</a></li>
-            <li><a href="preferences.php"><i class="fa fa-sliders fa-fw"></i>Preferences</a></li>
-            <li><a href="login.php"><i class="fa fa-eject fa-fw"></i>Sign Out</a></li>
-          </ul>  
-        </nav>
-      </div>
+  <body>
+  <?php include_once "parts/sidebar.php" ?>
       <!-- Main content --> 
       <div class="templatemo-content col-1 light-gray-bg">
         <div class="templatemo-top-nav-container">
